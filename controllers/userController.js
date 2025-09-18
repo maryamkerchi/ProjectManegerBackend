79102e9f4ecc0c53ff66f5d615aa220bb2dca63e
maryam <maryamkerchi@gmail.com>
2025-09-18 11:55:25 +0200

    user backend part

---
+++ controllers/userController.js
@@ -0,0 +1,121 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+// make JWT
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+// register user
+export const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, role, avatar } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      role: role || "user",
+      avatar,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+      avatar: user.avatar,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// login user
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+        avatar: user.avatar,
+        token: generateToken(user._id, user.role),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get all users list (just admin)
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//  get user detail
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (user) res.json(user);
+    else res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// update user info
+export const updateUser = async (req, res) => {
+  try {
+    const { firstName, lastName, role, avatar } = req.body;
+
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.role = role || user.role;
+    user.avatar = avatar || user.avatar;
+
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete user
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    await user.deleteOne();
+    res.json({ message: "User removed" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ routers/userRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// register and login
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// user managment
+router.get("/", protect, admin, getUsers);
+router.get("/:id", protect, getUserById);
+router.put("/:id", protect, updateUser);
+router.delete("/:id", protect, admin, deleteUser);
+
+export default router;
