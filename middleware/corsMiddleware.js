83715b7b1f494ed3b7e3d538993dd70334eabc1f
maryam <maryamkerchi@gmail.com>
2025-09-29 17:36:29 +0200

    edit cors middleware

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,10 +1,17 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: [
-    "http://localhost:5174",
-    "https://projectmanagementfrontend-spda.onrender.com",
-  ],
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (
+      origin.startsWith("http://localhost:") ||
+      origin === "https://projectmanagementfrontend-spda.onrender.com"
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 });
